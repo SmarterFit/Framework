44aed477419a6910e0f805d2923c703df7eed0ce
Pedro Lucas <pedrolucas.jsrn@gmail.com>
2025-07-17 13:09:47 -0300

    🐛 Updated origin

--- src/main/java/com/framework/modules/useraccess/controller/AuthController.java
+++ src/main/java/com/framework/modules/useraccess/controller/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin("*")
 public class AuthController {
 
     private final AuthService authService;
